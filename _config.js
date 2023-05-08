a4242d126be7a5aea4f19292d4b27bed3a80a141
root <root@beaglebone.localdomain>
2023-05-08 20:43:34 +0000

    (need to debug) added _config.js file for Eaton UDP API, plotting EMCB power instead of random data

---
+++ _config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    broadcast: [1,
+                2],
+    unicast: {
+        'device_ID_1': [3,
+                        4],
+        'device_ID_2': [5,
+                        6],
+        'device_ID_3': [7,
+                        8],
+        'device_ID_4': [9,
+                        10]
+    }
+};
